a0c3360cd687cf62eef27126501ff1b338811410
Josh <j.lynch@bboxx.co.uk>
2019-05-22 16:09:05 +0100

    Added send method to AfricasTalkingProvider
    - Created AfricasTalkingProviderError
    - Updated README with init, config & sending instructions

--- smsframework_africastalking/provider.py
+++ smsframework_africastalking/provider.py
@@ -1,5 +1,8 @@
 import africastalking
+import phonenumbers
 from smsframework import IProvider
+
+from .error import AfricasTalkingProviderError
 
 
 class AfricasTalkingProvider(IProvider):
@@ -14,7 +17,46 @@ class AfricasTalkingProvider(IProvider):
         :param username: (str) Username for your AfricasTalking account
         :param api_key: (str) API key for your AfricasTalking account
         """
-        super(gateway, name)
+        super().__init__(gateway, name)
 
         africastalking.initialize(username, api_key)
         self.sms_client = africastalking.SMS
+
+    def send(self, message):
+        """
+        Send a single text message.
+        TODO: Add a batch send feature.
+
+        :param message: (smsframework.OutgoingMessage) The message to send.
+        :return: (smsframework.OutgoingMessage) The sent message, updated with msgid.
+        """
+
+        try:
+            phone_number = phonenumbers.parse(
+                message.dst,
+                message.provider_params['target_country']
+            )
+
+            formatted_number = phonenumbers.format_number(
+                phone_number,
+                phonenumbers.PhoneNumberFormat.E164
+            )
+        except:
+            raise AfricasTalkingProviderError('PHONE_NUMBER_PARSE_ERROR')
+
+        try:
+            api_response = self.sms_client.send(
+                message.body,
+                [formatted_number],
+                message.provider_options.senderId
+            )
+
+            sent_message = api_response['SMSMessageData']['Recipients'][0]
+
+            if sent_message['status'] != 'Success':
+                raise Exception
+        except:
+            raise AfricasTalkingProviderError('API_ERROR')
+
+        message.msgid = sent_message['messageId']
+        return message
